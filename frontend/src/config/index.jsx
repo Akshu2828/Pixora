fd253c826a8a2fa14370bac8291e85a5522d7936
Akshay Gangurde <agangurde771@gmail.com>
2025-04-07 11:38:22 +0530

    added backend url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://https://pixora-65rs.onrender.com:9000";
+export const BASE_URL = "http://https://pixora-65rs.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
